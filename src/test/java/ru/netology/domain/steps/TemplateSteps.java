d9cf0ba07d87c6ce4e63ef6c6da1b17ade09b071
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-04-03 14:53:05 +0700

    Initial commit

---
+++ src/test/java/ru/netology/domain/steps/TemplateSteps.java
@@ -0,0 +1,58 @@
+package ru.netology.domain.steps;
+
+import com.codeborne.selenide.Selenide;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Пусть;
+import io.cucumber.java.ru.Тогда;
+import ru.netology.domain.data.DataHelper;
+import ru.netology.domain.page.DashboardPage;
+import ru.netology.domain.page.LoginPage;
+import ru.netology.domain.page.TransferPage;
+import ru.netology.domain.page.VerificationPage;
+
+import static ru.netology.domain.data.DataHelper.getFirstCardInfo;
+
+
+public class TemplateSteps {
+    private static LoginPage loginPage;
+    private static DashboardPage dashboardPage;
+    private static VerificationPage verificationPage;
+    private static TransferPage transferPage;
+    private static DataHelper.CardInfo data;
+
+    @Пусть("открыта страница с формой авторизации {string}")
+    public void openAuthPage(String url) {
+        loginPage = Selenide.open(url, LoginPage.class);
+    }
+
+    @Когда("пользователь пытается авторизоваться с именем {string} и паролем {string}")
+    public void loginWithNameAndPassword(String login, String password) {
+        verificationPage = loginPage.validLogin(login, password);
+    }
+
+    @И("пользователь вводит проверочный код 'из смс' {string}")
+    public void setValidCode(String verificationCode) {
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Тогда("происходит успешная авторизация и пользователь попадает на страницу 'Личный кабинет'")
+    public void verifyDashboardPage() {
+        dashboardPage.verifyIsDashboardPage();
+    }
+
+    @Тогда("появляется ошибка о неверном коде проверки")
+    public void verifyCodeIsInvalid() {
+        verificationPage.verifyCodeIsInvalid();
+    }
+
+    @И("осуществляется выбор карты")
+    public void selectCard(String card) {
+        dashboardPage.selectCardToTransfer(card);
+    }
+
+    @Тогда("происходит перевод с карты на карту с суммой {string} и номером карты {string}")
+    public void transfer(String amount, String cardNumber) {
+        transferPage.makeTransfer(amount, cardNumber);
+    }
+}
